aed604731d2119dfe1f429f7f8c7a82d170f554b
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-11-26 16:27:16 +0100

    added footer and new page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,10 +2,14 @@
 import React, { useState } from "react";
 
 import HomePage from "./_components/HomepageComponents/Homepage";
+import ProjectsList from "./_components/HomepageComponents/ProjectsList";
+import Footer from "./_components/HomepageComponents/Footer.js";
 const Home = () => {
   return (
     <>
       <HomePage />
+      <ProjectsList />
+      <Footer />
     </>
   );
 };
