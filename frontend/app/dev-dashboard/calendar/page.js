2cdc25d21b0c4d1387fe1c85d625d23462346e8e
AliRezahassnloo2731 <reza1632731@gmail.com>
2024-12-01 15:55:39 +0100

    fix: final version of Calendar.js; add it to both dev and client sites

---
+++ frontend/app/dev-dashboard/calendar/page.js
@@ -0,0 +1,11 @@
+import Calendar from "@/app/_components/client-dashboard-components/Calendar";
+
+function CalendarPage() {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+}
+
+export default CalendarPage;
