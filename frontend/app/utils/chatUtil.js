cb6ffbeda686869ca34a385486f2f428674e0125
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-12-03 23:50:54 +0100

    add getClientNames function

--- frontend/app/utils/chatUtil.js
+++ frontend/app/utils/chatUtil.js
@@ -7,3 +7,19 @@ export const getAllMessagesFromDeveloper = async developerId => {
     toast.error('Failed to fetch clients');
   });
 };
+
+export const getClientNames = async clientIds => {
+  const clientPromises = clientIds.map(async client => {
+    const response = await fetch(`/api/users/${client.id}`);
+    const result = await handleResponse(response, defaultSuccessCallback, () => {});
+    if (result && result.id && result.name) {
+      return { id: result.id, name: result.name };
+    }
+    return null;
+  });
+
+  const clients = await Promise.all(clientPromises);
+  const filteredClients = clients.filter(client => client !== null && client !== undefined);
+
+  return filteredClients;
+};
