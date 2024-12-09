af2c0d87ee290dc3e91a5599b2ad7a81dd7afb5b
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-12-09 22:07:33 +0100

    Added payment button, and payment tab. -the amount in the payment component is to be changed to the project amount -and the button is to be moved inside the "single project properties page" -Ale keeps working on the backend

--- frontend/app/dev-dashboard/page.js
+++ frontend/app/dev-dashboard/page.js
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { getFieldFromCookie } from '../utils/auth';
-import { getAllProjectsFromDeveloper } from '../utils/projectUtil';
-import { useRouter } from 'next/navigation';
-import DevProjectsList from '../_components/DevDashboard/DevProjectsList';
-import BookEvent from '../_components/DevDashboard/BookEvent';
-import ProjectCard from '../_components/DevDashboard/ProjectCard';
-import ChatCard from '../_components/DevDashboard/ChatCard';
+import { useEffect, useState } from "react";
+import { getFieldFromCookie } from "../utils/auth";
+import { getAllProjectsFromDeveloper } from "../utils/projectUtil";
+import { useRouter } from "next/navigation";
+import DevProjectsList from "../_components/DevDashboard/DevProjectsList";
+import BookEvent from "../_components/DevDashboard/BookEvent";
+import ProjectCard from "../_components/DevDashboard/ProjectCard";
+import ChatCard from "../_components/DevDashboard/ChatCard";
 
 const DevDashboard = () => {
   const [projects, setProjects] = useState([]);
@@ -18,7 +18,7 @@ const DevDashboard = () => {
 
   useEffect(() => {
     const fetchProjects = async () => {
-      const userId = getFieldFromCookie('userId');
+      const userId = getFieldFromCookie("userId");
 
       const allProjects = await getAllProjectsFromDeveloper(userId);
 
@@ -34,16 +34,25 @@ const DevDashboard = () => {
   }, []);
 
   return (
-    <div className='flex flex-col md:gap-6 gap-4 my-4 p-4 mx-auto max-w-4xl '>
-      <div className='flex md:flex-row flex-col gap-4'>
-        <div className='flex-1 hover:shadow-xl' onClick={() => router.push('/dev-dashboard/chat')}>
+    <div className="flex flex-col md:gap-6 gap-4 my-4 p-4 mx-auto max-w-4xl ">
+      <div className="flex md:flex-row flex-col gap-4">
+        <div
+          className="flex-1 hover:shadow-xl"
+          onClick={() => router.push("/dev-dashboard/chat")}
+        >
           <ChatCard />
         </div>
 
-        <div className='flex-1 hover:shadow-xl' onClick={() => router.push('/dev-dashboard/projects')}>
+        <div
+          className="flex-1 hover:shadow-xl"
+          onClick={() => router.push("/dev-dashboard/projects")}
+        >
           <ProjectCard />
         </div>
-        <div className='flex-1 hover:shadow-xl' onClick={() => router.push('/dev-dashboard/calendar')}>
+        <div
+          className="flex-1 hover:shadow-xl"
+          onClick={() => router.push("/dev-dashboard/calendar")}
+        >
           <BookEvent />
         </div>
       </div>
